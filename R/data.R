e96a1ab9c9a1864f538f500748ae675aca42d149
Wade Rogers <rogersw@upenn.edu>
2019-12-18 16:14:48 -0500

    Documenting the example dataset in roxygen2

--- R/data.R
+++ R/data.R
@@ -1,13 +1,16 @@
 #' A sampled flowSet
-#'
+#' @title A sampled flowSet
+#' @description
 #' This flowSet originated from data in
-#' \url https://flowrepository.org/id/FR-FCM-ZZGS
-#'
-#' The data were gated on singlet live events.  Then, each flowFrame in the
+#' \url{https://flowrepository.org/id/FR-FCM-ZZGS}.
+#' The data were first compensated and then transformed using a custom biexponential transform.
+#' They were then gated on singlet live events.  Then, each flowFrame in the
 #' flowSet was sampled to 10,000 events.  Finally, only young (age <= 35 yrs)
 #' was retained.
+#'     All this was done to reduce the size of the dataset for demonstration purposes.
 #'
 #' @format A flowSet with 43 flowFrames.
 #'
-#' @source "data/sampled_flowset_young.rda"
-#' @export "fs_young"
+#' @source \url{https://flowrepository.org/id/FR-FCM-ZZGS}
+sampled_flowset_young = "inst/extdata/sampled_flowset_young.rda"
+
