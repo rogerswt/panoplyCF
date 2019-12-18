9e705c6e2f1b342a52aac9d7faa29d2d110a81e7
Wade Rogers <rogersw@upenn.edu>
2019-12-18 14:16:24 -0500

    Trying to learn how to document a data set

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' A sampled flowSet
+#'
+#' This flowSet originated from data in
+#' \url https://flowrepository.org/id/FR-FCM-ZZGS
+#'
+#' The data were gated on singlet live events.  Then, each flowFrame in the
+#' flowSet was sampled to 10,000 events.  Finally, only young (age <= 35 yrs)
+#' was retained.
+#'
+#' @format A flowSet with 43 flowFrames.
+#'
+#' @source "data/sampled_flowset_young.rda"
+#' @export "fs_young"
