27d181e786b53f1aebd0e89664ee49b0a98487ff
Wade Rogers <rogersw@upenn.edu>
2019-12-19 13:19:01 -0500

    still trying to solve the vignette problem

--- R/panoply_methods.R
+++ R/panoply_methods.R
@@ -70,7 +70,7 @@ panoply = function(fcs, parameters = NULL, nRecursions = 12, perplexity = 40, nc
   if (is.null(parameters)) {
     stop("Parameters must be either a numeric or character vector\n")
     if (is.numeric(parameters)) {
-      parameters = flowCore::colnames(fcs)[parameters]
+      parameters = flowCore::colnames(ff)[parameters]
     }
   }
 
@@ -124,7 +124,7 @@ decorate_sample_panoply = function(fcs, panoply_obj, superclus=NULL,
     ff = fcs
   } else if (is(fcs, "flowSet")) {
     ff = suppressWarnings(as(fcs, "flowFrame"))
-    exprs(ff) = exprs(ff)[,which(colnames(ff) != "Original")]
+    flowCore::exprs(ff) = flowCore::exprs(ff)[,which(flowCore::colnames(flowCore::exprs(ff)) != "Original")]
   } else {
     stop("Argument fcs must either be a flowFrame or a flowSet\n")
   }
