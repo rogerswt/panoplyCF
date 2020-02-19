bdb7817fb1149eb98fea2e7b385d077148fdd641
Wade Rogers <rogersw@upenn.edu>
2020-02-19 15:16:43 -0500

    package configuration and documentation

--- R/data.R
+++ R/data.R
@@ -1,16 +1,22 @@
-#' A sampled flowSet
-#' @title A sampled flowSet
+#' A Pair of Sampled flowSets
+#'
+#' @name example_data
+#'
 #' @description
-#' This flowSet originated from data in
+#' These flowSets originated from data in
 #' \url{https://flowrepository.org/id/FR-FCM-ZZGS}.
-#' The data were first compensated and then transformed using a custom biexponential transform.
+#'
+#' The data were first compensated and then transformed using a custom biexponential transform
+#' using \href{https://github.com/rogerswt/wadeTools}{wadeTools}.
 #' They were then gated on singlet live events.  Then, each flowFrame in the
 #' flowSet was sampled to 10,000 events.  Finally, only young (age <= 35 yrs)
 #' was retained.
-#'     All this was done to reduce the size of the dataset for demonstration purposes.
 #'
-#' @format A flowSet with 43 flowFrames.
+#' All this was done to reduce the size of the dataset for demonstration purposes.
+#'
+#' @format FlowSets with 10 sampled flowFrames each.
 #'
 #' @source \url{https://flowrepository.org/id/FR-FCM-ZZGS}
-sampled_flowset_young = "inst/extdata/sampled_flowset_young.rda"
+NULL
+
 
